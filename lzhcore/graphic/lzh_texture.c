191069090eadeecc05f3bc836a614584c91a6515
huowenjie <huowenjie0428@sina.com>
2024-04-12 14:08:30 +0800

    no

---
+++ lzhcore/graphic/lzh_texture.c
@@ -0,0 +1,17 @@
+#include <lzh_mem.h>
+#include <SDL2/SDL_image.h>
+
+#include "lzh_texture.h"
+
+/*===========================================================================*/
+
+LZH_TEXTURE *lzh_texture_create(const char *path)
+{
+    return NULL;
+}
+
+void lzh_texture_destroy(LZH_TEXTURE *texture)
+{
+}
+
+/*===========================================================================*/
---
+++ lzhcore/graphic/lzh_texture.h
@@ -0,0 +1,23 @@
+#ifndef __LZH_TEXTURE_H__
+#define __LZH_TEXTURE_H__
+
+#include "lzh_opengl.h"
+
+/*===========================================================================*/
+/* Œ∆¿Ì */
+/*===========================================================================*/
+
+typedef struct LZH_TEXTURE
+{
+    GLuint  textureId;	
+    GLint   format;
+    int     width;
+    int     height;
+} LZH_TEXTURE;
+
+LZH_TEXTURE *lzh_texture_create(const char *path);
+void lzh_texture_destroy(LZH_TEXTURE *texture);
+
+/*===========================================================================*/
+
+#endif /* __LZH_TEXTURE_H__ */
